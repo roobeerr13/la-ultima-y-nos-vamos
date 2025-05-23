d1881ee0235c635770ad41f335aa69148189768b
Roberto Jimenez <robertojj2006@gmail.com>
2025-05-23 09:57:59 +0200

    ya esta

--- src/app.py
+++ src/app.py
@@ -1,22 +1,19 @@
-from .controllers.cli_controller import CLIController
-from .controllers.ui_controller import UIController
-from .services.poll_service import PollService
-from .services.user_service import UserService
-from .services.nft_service import NFTService
-from .repositories.encuesta_repo import PollRepository
-from .repositories.usuario_repo import UserRepository
-from .repositories.nft_repo import NFTRepository
-from .patterns.strategy import RandomTieBreaker
-from .ui.gradio_app import create_ui
 import sys
+from src.repositories.encuesta_repo import PollRepository
+from src.repositories.usuario_repo import UserRepository
+from src.repositories.nft_repo import NFTRepository
+from src.patterns.strategy import RandomTieBreaker
+from src.services.poll_service import PollService
+from src.services.user_service import UserService
+from src.services.nft_service import NFTService
+from src.controllers.cli_controller import CLIController
+from src.controllers.ui_controller import UIController
+from src.ui.gradio_app import create_ui
 
 def main():
-    file_path = "data/polls.json"
-    poll_repo = PollRepository(file_path)
-    user_file_path = "data/users.json"
-    user_repo = UserRepository(user_file_path)
-    nft_file_path = "data/nfts.json"
-    nft_repo = NFTRepository(nft_file_path)
+    poll_repo = PollRepository()
+    user_repo = UserRepository()
+    nft_repo = NFTRepository()
     tie_breaker = RandomTieBreaker()
     poll_service = PollService(poll_repo, tie_breaker)
     user_service = UserService(user_repo)
@@ -30,6 +27,3 @@ def main():
         ui.launch(show_error=True, debug=True)
     else:
         cli_controller.cmdloop()
-
-if __name__ == "__main__":
-    main()
